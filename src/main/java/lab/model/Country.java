4b3aa72886f4067c90b2275e7d44a648aa691819
Andrei Elagin <andrei_elagin@epam.com>
2017-10-16 01:01:57 +0300

    first commit plus little refactoring

---
+++ src/main/java/lab/model/Country.java
@@ -0,0 +1,29 @@
+package lab.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class Country implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    private int id;
+    private String name;
+    private String codeName;
+
+    public Country() {
+    }
+
+    public Country(int id, String name, String codeName) {
+        this.id = id;
+        this.name = name;
+        this.codeName = codeName;
+    }
+
+    public Country(String name, String codeName) {
+        this.name = name;
+        this.codeName = codeName;
+    }
+
+}
