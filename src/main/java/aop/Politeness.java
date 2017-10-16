48860be1a9713229d291e183aee06590be81465d
Andrei Elagin <andrei_elagin@epam.com>
2017-10-16 20:13:02 +0300

    Lab-5 aop completed

--- src/main/java/aop/Politeness.java
+++ src/main/java/aop/Politeness.java
@@ -1,11 +1,13 @@
 package aop;
 
-
 import lab.model.Customer;
 import lab.model.Squishee;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 
@@ -13,8 +15,8 @@ import org.aspectj.lang.annotation.Before;
 public class Politeness {
 
     @Before("execution(* sellSquishee(..))")
-    public void sayHello(JoinPoint joinPiont) {
-        AopLog.append("Hello " + ((Customer) joinPiont.getArgs()[0]).getName() + ". How are you doing? \n");
+    public void sayHello(JoinPoint joinPoint) {
+        AopLog.append("Hello " + ((Customer) joinPoint.getArgs()[0]).getName() + ". How are you doing? \n");
     }
 
     @AfterReturning(pointcut = "execution(* sellSquishee(..))",
@@ -23,14 +25,17 @@ public class Politeness {
         AopLog.append("Is " + ((Squishee) retVal).getName() + " Good Enough? \n");
     }
 
+    @AfterThrowing("execution(* sellSquishee(..))")
     public void sayYouAreNotAllowed() {
         AopLog.append("Hmmm... \n");
     }
 
+    @After("execution(* sellSquishee(..))")
     public void sayGoodBye() {
         AopLog.append("Good Bye! \n");
     }
 
+    @Around("execution(* sellSquishee(..))")
     public Object sayPoliteWordsAndSell(ProceedingJoinPoint pjp) throws Throwable {
         AopLog.append("Hi! \n");
         Object retVal = pjp.proceed();
