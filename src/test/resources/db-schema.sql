4ce2537556b0351ecbc47e86236c0daacf24c634
Andrei Elagin <andrei_elagin@epam.com>
2017-10-30 02:27:01 +0300

    Lab-6 jdbc completed

---
+++ src/test/resources/db-schema.sql
@@ -0,0 +1,6 @@
+create table country( id identity , name varchar (255) , code_name varchar (255) );
+INSERT INTO country(name, code_name) 
+VALUES('Australia', 'AU'), ( 'Canada', 'CA' ), ( 'France', 'FR' ), 
+( 'Hong Kong', 'HK' ), ( 'Iceland', 'IC' ), ( 'Japan', 'JP' ), 
+( 'Nepal', 'NP' ), ( 'Russian Federation', 'RU' ), ( 'Sweden', 'SE' ), 
+( 'Switzerland', 'CH' ), ( 'United Kingdom', 'GB' ), ( 'United States', 'US' );
