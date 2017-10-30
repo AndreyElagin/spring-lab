cd96017a0d6b317224d05f8f26998d4b924efa9c
Andrei Elagin <andrei_elagin@epam.com>
2017-10-30 09:53:45 +0300

    WIP Lab-7 orm partly imported work code

--- src/main/java/lab/model/Country.java
+++ src/main/java/lab/model/Country.java
@@ -2,14 +2,24 @@ package lab.model;
 
 import lombok.Data;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
 @Data
+@Entity
+@Table(name = "Country")
 public class Country implements Serializable {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @Column(name = "NAME")
     private String name;
+
+    @Column(name = "CODE_NAME")
     private String codeName;
 
     public Country() {
